561e627549336eb1ba334306eef5cef89fed5251
hariom <hariomsain.india@gmail.com>
2026-03-16 20:04:19 +0530

    1. write file in chunk 2. crc32 checksum calculation

--- index.js
+++ index.js
@@ -5,16 +5,17 @@ const generateBtn = document.getElementById("generateBtn");
 const clearBtn = document.getElementById("clearBtn");
 const listContainer = document.getElementById("file-list");
 listContainer.innerHTML = "No file selected";
+const statusMsg = document.getElementById("status-msg");
 const zipper = new Zipper();
 
-const MAX_WRITE_SIZE = 10 * 1024.0;
+const MAX_WRITE_SIZE = 100000000; //100mb
 
 input.addEventListener("change", (event) => {
     if (input.files.length == 0) {
         listContainer.innerHTML = "No file selected"
         return;
     }
-    let table = "<table style='width: 100%;text-align:center;'><tr><th>Name</th><th>Type</th><th>Ext</th><th>Size</th></tr>";
+    let table = "<table style='width: 100%;text-align:center;'><tr><th>Name</th><th>Type</th><th>Ext</th><th>Size(bytes)</th></tr>";
     for (let i = 0; i < input.files.length; i++) {
         const file = input.files[i];
         console.log(file.name);
@@ -40,24 +41,28 @@ generateBtn.addEventListener("click", async () => {
         const size = file.size;
         const arr = calcuateSliceArray(size);
         const name = file.name;
+        if (arr.length == 1) {
+            statusMsg.innerHTML = "Adding: file: " + name;
+        }
         for (let i = 0; i < arr.length; i++) {
+            statusMsg.innerHTML = "Adding: " + name + " > Chunk (" + (i + 1) + "/" + arr.length + ")";
             const blob = file.slice(arr[i].start, arr[i].end);
             const buffer = await blob.arrayBuffer();
             const uint8View = new Uint8Array(buffer);
             if (i == 0) {
-                await zipper.writeFile(name, uint8View);
+                await zipper.writeFile(name, uint8View, new Date());
                 continue;
             }
             await zipper.appendToFile(uint8View)
         }
-        //console.log("file: ", file.name, ", slices: ", arr);
-        //    
-        //     const uint8FileData = await file.arrayBuffer();
     }
+    statusMsg.innerHTML = "Finalizing...";
     await zipper.done();
 
 
     input.value = null;
+    listContainer.innerHTML = "";
+    statusMsg.innerHTML = "Done";
 })
 
 function calcuateSliceArray(size) {
